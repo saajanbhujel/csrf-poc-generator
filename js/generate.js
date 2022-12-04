d6d51ffe316dac669867b5b9ca7a7bd3c3e2ce5b
Saajan Bhujel <87616854+saajanbhujel@users.noreply.github.com>
2022-12-04 15:32:17 +0530

    Update generate.js

--- js/generate.js
+++ js/generate.js
@@ -55,8 +55,8 @@ $("#generate").click(function() {
 	console.log(parametersValue);
 */
 
-	/*$("#poc").val(generateForm(requirements, parameters, parametersKey, parametersValue));*/
-	$("#highlighting-content").val(generateForm(requirements, parameters, parametersKey, parametersValue));
+	$("#poc").val(generateForm(requirements, parameters, parametersKey, parametersValue));
+	/*$("#highlighting-content").val(generateForm(requirements, parameters, parametersKey, parametersValue));*/
 });
 
 function generateForm(requirements, parameters, parametersKey, parametersValue) {
